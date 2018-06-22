8c1863822659b47da68774fd93a901ac4657e838
Ignacio Tripodi <ignaciot@gmail.com>
2018-06-22 13:47:44 -0600

    Fixed this script to work with any generic pipeline

--- bin/readcount_corrected_geneomeBedgraphs.py
+++ bin/readcount_corrected_geneomeBedgraphs.py
@@ -9,41 +9,30 @@ def calmp(num_of_reads, total_reads):
         return mp
 
 #Normalize bedgraphs to millions mapped reads
-def main(directory_of_sortedbams):
-    dic_mapped = {}
-    outdir = directory_of_sortedbams + "/genomecoveragebed/"
-    for sorted_bam_file_and_path in glob.glob(os.path.join(directory_of_sortedbams, '*sorted.bam.flagstat')):
-        bamfileroot = sorted_bam_file_and_path.split("/")[-1].split(".sorted")[0]
-        f = open(sorted_bam_file_and_path)
-        lines = f.readlines()
-        mapped_reads = int(lines[4].split(" ")[0])
-        dic_mapped[bamfileroot] = mapped_reads
-        f.close()
-    for bamfile in glob.glob(os.path.join(directory_of_sortedbams, '*.sorted.bam')):
-        bamfileroot = bamfile.split("/")[-1]
-        bamfileroot = bamfileroot.split(".sorted")[0]
-        bedgraph = outdir+"sorted."+bamfileroot+".sorted.sorted.BedGraph"
-        if not os.path.isfile(bedgraph):
-            bedgraph = outdir+bamfileroot+".sorted.sorted.BedGraph"
-        total_reads = dic_mapped[bamfileroot]
-        bedgraphout = bedgraph+".mp.BedGraph"
-        wf = open(bedgraphout, "w")
-        with open(bedgraph) as f:
-            for line in f:
-                line = line.strip('\n').split('\t')
-                if len(line)<3:
-                    try:
-                        line = line[0].split(" ")
-                    except:
-                        print line
-                chrom, start, stop, num_of_reads = line
-                frag = calmp(float(num_of_reads), total_reads)
-                newline = "\t".join([chrom, start, stop, str(frag)])+"\n"
-                wf.write(newline)
-        wf.close()
+def main(flagstat_file, sorted_bed_file):
+    bamfileroot = flagstat_file.split("/")[-1].split(".sorted")[0]
+    f = open(flagstat_file)
+    lines = f.readlines()
+    total_reads = int(lines[4].split(" ")[0])
+    f.close()
+    bedgraphout = '.'.join(sorted_bed_file.split('.')[:-1]) + '.mp.BedGraph'
+    wf = open(bedgraphout, "w")
+    with open(sorted_bed_file) as f:
+        for line in f:
+            line = line.strip('\n').split('\t')
+            if len(line)<3:
+                try:
+            	    line = line[0].split(" ")
+                except:
+            	    print line
+            chrom, start, stop, num_of_reads = line
+            frag = calmp(float(num_of_reads), total_reads)
+            newline = "\t".join([chrom, start, stop, str(frag)])+"\n"
+            wf.write(newline)
+    wf.close()
 
 
 
 if __name__=="__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
 
